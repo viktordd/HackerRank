2286a04af610536cea0fce826535686fbd32697f
Viktor Dikov <Viktordd@gmail.com>
2019-05-05 15:43:35 -0400

    Adding more solutions

---
+++ HackerRank/Arrays/MinimumSwaps2/Solution.cs
@@ -0,0 +1,72 @@
+﻿namespace HackerRank.Arrays.MinimumSwaps2
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MinimumSwaps2Tests
+    {
+        [DataTestMethod]
+        [DataRow(new[] { 8, 45, 35, 84, 79, 12, 74, 92, 81, 82, 61, 32, 36, 1, 65, 44, 89, 40, 28, 20, 97, 90, 22, 87, 48, 26, 56, 18, 49, 71, 23, 34, 59, 54, 14, 16, 19, 76, 83, 95, 31, 30, 69, 7, 9, 60, 66, 25, 52, 5, 37, 27, 63, 80, 24, 42, 3, 50, 6, 11, 64, 10, 96, 47, 38, 57, 2, 88, 100, 4, 78, 85, 21, 29, 75, 94, 43, 77, 33, 86, 98, 68, 73, 72, 13, 91, 70, 41, 17, 15, 67, 93, 62, 39, 53, 51, 55, 58, 99, 46 }, 91, 
+            DisplayName = "large 100, 91")]
+        [DataRow(new[] { 3, 2, 1, 5, 4, 8, 6, 7 }, 4, DisplayName = "[ 3, 2, 1, 5, 4, 8, 6, 7 ], 4")]
+        [DataRow(new[] { 7, 1, 3, 2, 4, 5, 6 }, 5, DisplayName = "[ 7, 1, 3, 2, 4, 5, 6 ], 5")]
+        [DataRow(new[] { 4, 3, 1, 2 }, 3, DisplayName = "[ 4, 3, 1, 2 ], 3")]
+        [DataRow(new[] { 2, 3, 4, 1, 5 }, 3, DisplayName = "[ 2, 3, 4, 1, 5 ], 3")]
+        [DataRow(new[] { 1, 3, 5, 2, 4, 6, 7 }, 3, DisplayName = "[ 1, 3, 5, 2, 4, 6, 7 ], 3")]
+        public void MergeSortCountingInversionsTest(int[] input, int expected)
+        {
+            var result = Solution.minimumSwaps(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+    class Solution
+    {
+        // Complete the minimumSwaps function below.
+        public static int minimumSwaps(int[] arr)
+        {
+            var numberSwaps = 0;
+            var i = 0;
+
+            while (i < arr.Length)
+            {
+                int finalPosition = arr[i] - 1;
+
+                if (finalPosition != i)
+                {
+                    int buffer = arr[i];
+                    arr[i] = arr[finalPosition];
+                    arr[finalPosition] = buffer;
+                    numberSwaps++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return numberSwaps;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+            int res = minimumSwaps(arr);
+
+            textWriter.WriteLine(res);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+
+}
---
+++ HackerRank/Sorting/DictionariesAndHashmaps/Solution.cs
@@ -0,0 +1,120 @@
+﻿namespace HackerRank.Sorting.DictionariesAndHashmaps
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
+
+    [TestClass]
+    public class MergeSortCountingInversionsTests
+    {
+        [DataTestMethod]
+        [DataRow("[ [1, 5], [3, 2], [1, 5], [3, 2], [2, 5], [3, 1], [2, 5], [3, 1] ]", new[] { 0, 1, 1, 0 },
+            DisplayName = "[ [1, 5], [3, 2], [1, 5], [3, 2], [2, 5], [3, 1], [2, 5], [3, 1] ], [0, 1, 1, 0]")]
+        [DataRow("[ [1, 5], [1, 6], [3, 2], [1, 10], [1, 10], [1, 6], [2, 5], [3, 2] ]", new[] { 0, 1 },
+            DisplayName = "[ [1, 5], [1, 6], [3, 2], [1, 10], [1, 10], [1, 6], [2, 5], [3, 2] ], [0, 1]")]
+        [DataRow("[ [3, 4], [2, 1003], [1, 16], [3, 1] ]", new[] { 0, 1 },
+            DisplayName = "[ [3, 4], [2, 1003], [1, 16], [3, 1] ], [ 0, 1 ]")]
+        [DataRow("[ [1, 3], [2, 3], [3, 2], [1, 4], [1, 5], [1, 5], [1, 4], [3, 2], [2, 4], [3, 2] ]", new[] { 0, 1, 1 },
+            DisplayName = "[ [1, 3], [2, 3], [3, 2], [1, 4], [1, 5], [1, 5], [1, 4], [3, 2], [2, 4], [3, 2] ], [ 0, 1, 1 ]")]
+        public void MergeSortCountingInversionsTest(string input, int[] expected)
+        {
+            var list = JsonConvert.DeserializeObject<List<int[]>>(input);
+            var result = Solution.freqQuery(list);
+
+            Assert.AreEqual(expected.Length, result.Count);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+    }
+
+    class Solution
+    {
+
+        // Complete the freqQuery function below.
+        public static List<int> freqQuery(List<int[]> queries)
+        {
+            var result = new List<int>();
+
+            var data = new Dictionary<int, int>();
+            var freq = new Dictionary<int, int>();
+
+            foreach (var query in queries)
+            {
+                var val = query[1];
+                switch (query[0])
+                {
+                    case 1: Insert(data, freq, val); break;
+                    case 2: Delete(data, freq, val); break;
+                    case 3: //Check
+                        result.Add(freq.TryGetValue(val, out int f) && f > 0 ? 1 : 0);
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        private static void Insert(Dictionary<int, int> data, Dictionary<int, int> freq, int val)
+        {
+            if (data.TryGetValue(val, out int count))
+            {
+                if (count > 0)
+                    freq[count]--;
+                data[val] = ++count;
+                AddFrequency(freq, count);
+            }
+            else
+            {
+                data[val] = 1;
+                AddFrequency(freq, 1);
+            }
+        }
+
+        private static void Delete(Dictionary<int, int> data, Dictionary<int, int> freq, int val)
+        {
+            if (data.TryGetValue(val, out int ct) && ct > 0)
+            {
+                freq[ct]--;
+                data[val] = --ct;
+                if (ct > 0)
+                    AddFrequency(freq, ct);
+            }
+        }
+
+        private static void AddFrequency(Dictionary<int, int> freq, int f)
+        {
+            if (freq.ContainsKey(f))
+                freq[f]++;
+            else
+                freq[f] = 1;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int[]> queries = new List<int[]>();
+
+            for (int i = 0; i < q; i++)
+            {
+                queries.Add(Console.ReadLine().TrimEnd().Split(' ').Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToArray());
+            }
+
+            List<int> ans = freqQuery(queries);
+
+            textWriter.WriteLine(String.Join("\n", ans));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
