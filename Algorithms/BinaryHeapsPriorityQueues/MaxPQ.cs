479414584b8a2225a438d5f35ccb596eb1abc538
Viktor Dikov <Viktordd@gmail.com>
2019-06-02 20:40:59 -0400

    Add algorightm implementations

---
+++ Algorithms/BinaryHeapsPriorityQueues/MaxPQ.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    //Considerations
+    //Immutability of keys.
+    //・Assumption: client does not change keys while they're on the PQ.
+    //・Best practice: use immutable keys.
+    //Underflow and overflow.
+    //・Underflow: throw exception if deleting from empty PQ.
+    //・Overflow: add no-arg constructor and use resizing array.
+    //Minimum-oriented priority queue.
+    //・Replace less() with greater().
+    //・Implement greater().
+    //Other operations.
+    //・Remove an arbitrary item.
+    //・Change the priority of an item.
+
+
+    /// <summary>
+    /// Max Priority Queue implemented with Binary heap
+    /// </summary>
+    public class MaxPQ<T> where T : IComparable<T>
+    {
+        private readonly List<T> pq;
+
+        public MaxPQ()
+        {
+            pq = new List<T>();
+        }
+        public bool IsEmpty()
+        {
+            return pq.Count == 0;
+        }
+
+        public void Insert(T x)
+        {
+            pq.Add(x);
+            Swim(pq.Count - 1);
+        }
+
+        public T DelMax()
+        {
+            T max = pq[0];
+            Exchange(0, pq.Count - 1);
+            pq.RemoveAt(pq.Count - 1);
+            Sink(0);
+            return max;
+        }
+
+        private void Swim(int k)
+        {
+            while (k > 0 && Less(Parent(k), k))
+            {
+                int parent = Parent(k);
+                Exchange(k, parent);
+                k = parent;
+            }
+        }
+
+        private void Sink(int k)
+        {
+            while (LChild(k) < pq.Count)
+            {
+                int j = LChild(k);
+                if (j + 1 < pq.Count && Less(j, j + 1)) j++;
+                if (!Less(k, j)) break;
+                Exchange(k, j);
+                k = j;
+            }
+        }
+
+        private int Parent(int k) => (k - 1) / 2;
+
+        private int LChild(int k) => 2 * k + 1;
+
+        private bool Less(int i, int j)
+        {
+            return pq[i].CompareTo(pq[j]) < 0;
+        }
+
+        private void Exchange(int i, int j)
+        {
+            T t = pq[i];
+            pq[i] = pq[j];
+            pq[j] = t;
+        }
+    }
+}
---
+++ Algorithms/UnionFind/QuickUnionUF.cs
@@ -0,0 +1,58 @@
+﻿namespace Algorithms
+{
+    /// <summary>
+    /// Weighted Quick-union Union Find with Path compression
+    /// algorithm worst-case time
+    /// -quick-find: M N
+    /// -quick-union: M N
+    /// -weighted QU: N + M log N
+    /// -QU + path compression: N + M log N
+    /// -weighted QU + path compression: N + M lg* N
+    /// *M union-ﬁnd operations on a set of N objects
+    /// </summary>
+    class QuickUnionUF
+    {
+        private readonly int[] id;
+        private readonly int[] size;
+
+        public QuickUnionUF(int n)
+        {
+            id = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++)
+                id[i] = i;
+        }
+
+        private int Root(int i)
+        {
+            while (i != id[i])
+            {
+                id[i] = id[id[i]];
+                i = id[i];
+            }
+            return i;
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return Root(p) == Root(q);
+        }
+
+        public void Union(int p, int q)
+        {
+            int i = Root(p);
+            int j = Root(q);
+            if (i == j) return;
+            if (size[i] < size[j])
+            {
+                id[i] = j;
+                size[j] += size[i];
+            }
+            else
+            {
+                id[j] = i;
+                size[i] += size[j];
+            }
+        }
+    }
+}
