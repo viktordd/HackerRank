ff1d0ea053dbaf869d2128d3f4d82377ab5cd1ea
Viktor Dikov <viktordd@gmail.com>
2023-01-22 15:32:54 -0500

    update templates to use System.Text.Json

--- Template.cs
+++ Template.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
+﻿using System.Text.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TemplateNamespace;
 
@@ -10,7 +10,7 @@ public class Template_Test
     [DataRow("", true)]
     public void Template_Solutions(string arg1Json, bool expected)
     {
-        var arg1 = JsonConvert.DeserializeObject<int[][]>(arg1Json);
+        var arg1 = JsonSerializer.Deserialize<int[][]>(arg1Json);
         var solution = new Template();
         var result = solution.Method(arg1);
         Assert.AreEqual(expected, result);
